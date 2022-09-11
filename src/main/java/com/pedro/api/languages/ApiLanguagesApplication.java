1371934fe16bc03a9f46a7c420773c9b03f514a4
Pedro Vicente Santos <pedrovicente.dev@gmail.com>
2022-09-11 16:52:12 -0300

    build: add config do use port from env var

--- src/main/java/com/pedro/api/languages/ApiLanguagesApplication.java
+++ src/main/java/com/pedro/api/languages/ApiLanguagesApplication.java
@@ -1,5 +1,7 @@
 package com.pedro.api.languages;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,10 +15,16 @@ public class ApiLanguagesApplication {
 			String mongodbUser = dotenv.get("MONGODB_USER");
 			String mongodbUDatabase = dotenv.get("MONGODB_DATABASE");
 
+			String port = dotenv.get("PORT") == null ? "8080" : dotenv.get("PORT");
+
 			String mongodbUri = String.format("mongodb+srv://%s:%s@cluster0.b01ut.mongodb.net/%s?retryWrites=true&w=majority", mongodbUser, mongodbPassword, mongodbUDatabase);
 			System.setProperty("spring.data.mongodb.uri", mongodbUri);
 
-			SpringApplication.run(ApiLanguagesApplication.class, args);
+			SpringApplication app = new SpringApplication(ApiLanguagesApplication.class);
+
+			app.setDefaultProperties(Collections.singletonMap("server.port", port));
+
+			app.run(args);
 	}
 
 }
